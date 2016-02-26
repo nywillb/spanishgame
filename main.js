5204a9e691a1e73036dc888220b70e9b94557c38
nywillb <william@barkoffusa.com>
2016-02-26 17:45:43 +0000

    Make it work

---
+++ main.js
@@ -0,0 +1,14 @@
+var cards = [
+    "Card 1", 
+    "Card 2",
+    "Card 3"
+    ];
+
+function newcard(){
+    var cardnum = Math.floor((Math.random() * cards.length) + 1);
+    document.getElementById("card").innerHTML = cardnum;
+}
+
+window.onkeypress = function(event) {
+    newcard();
+};
